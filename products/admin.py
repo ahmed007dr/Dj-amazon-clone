5bb417d2f4bd9279e8b621a4315503e45aec1b49
ahmed007dr <ahmed007eg@gmail.com>
2023-12-05 18:29:40 +0200

    add models to admin . add tabular inline

--- products/admin.py
+++ products/admin.py
@@ -3,8 +3,15 @@ from .models import Product,Brand,Review,ProductsImage
 # Register your models here.
 
 
+class ProductImageinline(admin.TabularInline):
+    model=ProductsImage
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines=[ProductImageinline]
+
+# alot of upload
+
+admin.site.register(Product,ProductAdmin)
 admin.site.register(Brand)
 admin.site.register(Review)
-admin.site.register(ProductsImage)
+#admin.site.register(ProductsImage)  # remove one upload image
