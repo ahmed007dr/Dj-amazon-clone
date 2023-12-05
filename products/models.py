ad17ec9b5e74106d78484abfad2ddf81232d7e43
ahmed007dr <ahmed007eg@gmail.com>
2023-12-05 17:09:39 +0200

    add translation to product models

--- products/models.py
+++ products/models.py
@@ -4,6 +4,7 @@ from taggit.managers import TaggableManager
 from django.contrib.auth.models import User # to make relation with django user consibt
 from django.utils import timezone #timenow
 from django.utils.text import slugify # convert name to url
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
 FLAG_TYPES=(
     ('New','New'),
@@ -11,17 +12,17 @@ FLAG_TYPES=(
     ('feature','feature')
 ) #('data','display')
 
-
+#,verbose_name=_('')
 class Product(models.Model):
-    name=models.CharField(max_length=120)
-    flag=models.CharField(max_length=10,choices=FLAG_TYPES)
-    price=models.FloatField()
-    image=models.ImageField(upload_to='product')
-    sku=models.IntegerField()
-    subtitle=models.TextField(max_length=400)
-    description=models.TextField(max_length=50000)
-    brand=models.ForeignKey('Brand',related_name='Product_brand',on_delete=models.SET_NULL,null=True)#relation with
-    tags = TaggableManager()
+    name=models.CharField(_('name'),max_length=120)
+    flag=models.CharField(_('flag'),max_length=10,choices=FLAG_TYPES)
+    price=models.FloatField(_('price'))
+    image=models.ImageField(_('image'),upload_to='product')
+    sku=models.IntegerField(_('sku'))
+    subtitle=models.TextField(_('subtitle'),max_length=400)
+    description=models.TextField(_('description'),max_length=50000)
+    brand=models.ForeignKey('Brand',verbose_name=_('brand'),related_name='Product_brand',on_delete=models.SET_NULL,null=True)#relation with
+    tags = TaggableManager(_('tags'))
 
     slug=models.SlugField(blank=True,null=True) #take title convert to url
     #override
@@ -31,13 +32,13 @@ class Product(models.Model):
 
 
 class ProductsImage(models.Model):
-    product=models.ForeignKey(Product,related_name='product_imge',on_delete=models.CASCADE)#relation delete all cascade
-    imge=models.ImageField(upload_to='productimages')
+    product=models.ForeignKey(Product,verbose_name=_('product'),related_name='product_imge',on_delete=models.CASCADE)#relation delete all cascade
+    imge=models.ImageField(_('imge')upload_to='productimages')
     
 
 class Brand(models.Model):
-    name=models.CharField(max_length=100)
-    image=models.ImageField(upload_to='brand')
+    name=models.CharField(_('name')max_length=100)
+    image=models.ImageField(_("image"),upload_to='brand')
 
 
     slug=models.SlugField(blank=True,null=True) #take title convert to url
@@ -47,9 +48,9 @@ class Brand(models.Model):
         super(Brand,self).save(*args,**kwargs) 
 
 class Review(models.Model):
-    user=models.ForeignKey(User,related_name='review_user',on_delete=models.SET_NULL,null=True)#relation with user djago
-    product=models.ForeignKey(Product,related_name='review_product',on_delete=models.CASCADE) #relation المنتج مع التقيمات
-    review=models.TextField(max_length=500)
-    rate=models.IntegerField(choices=[(i,i)for i in range(1,6)])
+    user=models.ForeignKey(User,verbose_name=_('user'),related_name='review_user',on_delete=models.SET_NULL,null=True)#relation with user djago
+    product=models.ForeignKey(Product,verbose_name=_('product'),related_name='review_product',on_delete=models.CASCADE) #relation المنتج مع التقيمات
+    review=models.TextField(_("review"),max_length=500)
+    rate=models.IntegerField(_('rate'),choices=[(i,i)for i in range(1,6)])
     created_at=models.DateTimeField(default=timezone.now) # import untie
 
