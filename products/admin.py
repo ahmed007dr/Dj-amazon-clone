17d0a5eb0c29097efd3676a9d64b1587d9e8d451
ahmed007dr <ahmed007eg@gmail.com>
2023-12-05 17:35:13 +0200

    step befor runserver

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Product,Brand,Review,ProductsImage
 # Register your models here.
+
+
+
+admin.site.register(Product)
+admin.site.register(Brand)
+admin.site.register(Review)
+admin.site.register(ProductsImage)
