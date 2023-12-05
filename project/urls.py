5dc73f29f61a2ba934c404690e5a357aba7a862f
ahmed007dr <ahmed007eg@gmail.com>
2023-12-05 17:27:12 +0200

    add static and media  settings

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# to can make file static and media from browser 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)# add setting static from url
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # add setting for media url
