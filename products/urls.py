0e4f8a48b50ff4ccfad53d3b909b4a750cbd362c
ahmed007dr <ahmed007eg@gmail.com>
2023-12-13 23:29:52 +0200

    loop in brand list ( image & name)

--- products/urls.py
+++ products/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from .views import ProductList , ProductDetail , BrandList , BrandDetail
 
 urlpatterns = [
+    path('brands',BrandList.as_view()),
+    path('brands/<slug:slug>',BrandDetail.as_view()), 
     path('', ProductList.as_view()),
     path('<slug:slug>', ProductDetail.as_view()),
-    path('brand',BrandList.as_view()),
-    path('brands/<slug:slug>',BrandDetail.as_view())
+
 ]
+
