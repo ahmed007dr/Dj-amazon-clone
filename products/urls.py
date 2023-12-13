b927cbce5d328618d54daf1a316bb02cd0828b55
ahmed007dr <ahmed007eg@gmail.com>
2023-12-13 23:18:35 +0200

    handel brand_detail&list.html

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ProductList , ProductDetail
+from .views import ProductList , ProductDetail , BrandList , BrandDetail
 
 urlpatterns = [
     path('', ProductList.as_view()),
     path('<slug:slug>', ProductDetail.as_view()),
+    path('brand',BrandList.as_view()),
+    path('brands/<slug:slug>',BrandDetail.as_view())
 ]
