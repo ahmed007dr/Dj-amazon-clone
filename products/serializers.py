8a6c9b2d31f78523b18ca0d474d7bbb037925258
ahmed007dr <ahmed007eg@gmail.com>
2023-12-23 00:11:53 +0200

    show avg_rate in products API

--- products/serializers.py
+++ products/serializers.py
@@ -2,26 +2,52 @@ from rest_framework import serializers
 from .models import Product,Brand
 
 class ProductListSerializer(serializers.ModelSerializer):
-    brand=serializers.StringRelatedField()
-    review_count=serializers.SerializerMethodField(method_name="get_review_count")
+    brand=serializers.StringRelatedField() # to add column in api
+    review_count=serializers.SerializerMethodField()#method_name="get_review_count" # to add column in api
+    avg_rate=serializers.SerializerMethodField()#method_name="get_avg_rate" # to add column in api
     class Meta:
         model=Product
         fields='__all__'
 
     def get_review_count(self,object): #name of function (get_)+ name of column # self couz in class #object product activae
-        reviews=object.review_product.all().count()
+        reviews = object.review_product.all().count()
         return reviews
+    
+    def get_avg_rate(self,object):
+        total=0 #sum rate: object (one product)
+        reviews=object.review_product.all()
+        
+        if len(reviews)>0:
+            for item in reviews:
+                total+= item.rate
+            avg= total / len(reviews)
+        else:
+            avg=0
+        return avg
 
 class ProductDetailSerializer(serializers.ModelSerializer):
-    brand=serializers.StringRelatedField()
-    reviews=object.review_product.all().count()
+    brand=serializers.StringRelatedField() # to add column in api
+    reviews_count=serializers.SerializerMethodField() # to add column in api
+    avg_rate=serializers.SerializerMethodField()#method_name="get_avg_rate" # to add column in api
+
     class Meta:
         model=Product
         fields='__all__'
-
     def get_review_count(self,object): #name of function (get_)+ name of column # self couz in class #object product activae
-        reviews=object.review_product.all().count()
+        reviews = object.review_product.all().count()
         return reviews
+
+    def get_avg_rate(self,object):
+        total=0 #sum rate: object (one product)
+        reviews=object.review_product.all()
+        
+        if len(reviews)>0:
+            for item in reviews:
+                total+= item.rate
+            avg= total / len(reviews)
+        else:
+            avg=0
+        return avg
 
 
 
