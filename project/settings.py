aa3e9f481f227ecba6cea6d896627e8d4e190b15
ahmed007dr <ahmed007eg@gmail.com>
2023-12-22 18:46:16 +0200

    add product list & details , brand list & details API

--- project/settings.py
+++ project/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    
+    #packages
     'taggit',
+    #ur app
+    'rest_framework',
     'products',
 ]
 
