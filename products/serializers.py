aa79477d2684961d3ebd7cd3ea2c3a70f566248f
ahmed007dr <ahmed007eg@gmail.com>
2023-12-23 00:53:34 +0200

    catch some error captail and small word

--- products/serializers.py
+++ products/serializers.py
@@ -8,18 +8,19 @@ class ProductImagesSerializer(serializers.ModelSerializer):
 
 class ProductReviewSerializer(serializers.ModelSerializer):
     class Meta:
-        Model=Review
+        model=Review
         fields=['user','review','rate','created_at']
 
 class ProductListSerializer(serializers.ModelSerializer):
-    brand=serializers.StringRelatedField() # to add column in api
-    review_count=serializers.SerializerMethodField()#method_name="get_review_count" # to add column in api
+    brand=serializers.StringRelatedField() # to add column in api 
+    reviews_count=serializers.SerializerMethodField()#method_name="get_review_count" # to add column in api #link def 
     avg_rate=serializers.SerializerMethodField()#method_name="get_avg_rate" # to add column in api
+    
     class Meta:
         model=Product
         fields='__all__'
 
-    def get_review_count(self,object): #name of function (get_)+ name of column # self couz in class #object product activae
+    def get_reviews_count(self,object): #name of function (get_)+ name of column # self couz in class #object product activae
         reviews = object.review_product.all().count()
         return reviews
     
@@ -45,9 +46,9 @@ class ProductDetailSerializer(serializers.ModelSerializer):
         model=Product
         fields='__all__'
 
-    # def get_review_count(self,object): #name of function (get_)+ name of column # self couz in class #object product activae
-    #     reviews = object.review_product.all().count()
-    #     return reviews
+    def get_reviews_count(self,object): #name of function (get_)+ name of column # self couz in class #object product activae
+        reviews = object.review_product.all().count()
+        return reviews
 
     def get_avg_rate(self,object):
         total=0 #sum rate: object (one product)
