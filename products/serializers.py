bba1dd862c7d1e8ef3a5710f17af529fa20f686e
ahmed007dr <ahmed007eg@gmail.com>
2023-12-22 23:42:36 +0200

    review count

--- products/serializers.py
+++ products/serializers.py
@@ -3,9 +3,14 @@ from .models import Product,Brand
 
 class ProductListSerializer(serializers.ModelSerializer):
     brand=serializers.StringRelatedField()
+    review_count=serializers.SerializerMethodField(method_name="get_review_count")
     class Meta:
         model=Product
         fields='__all__'
+
+    def get_review_count(self,object): #name of function (get_)+ name of column # self couz in class #object product activae
+        reviews=object.review_product.all().count()
+        return reviews
 
 class ProductDetailSerializer(serializers.ModelSerializer):
     brand=serializers.StringRelatedField()
