d0e1068d1288cb8882681922ba4b341b2e5e192e
ahmed007dr <ahmed007eg@gmail.com>
2023-12-22 19:39:46 +0200

    error with filter

--- products/api.py
+++ products/api.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 from .models import Product,Brand,ProductImage,Review
 from . import serializers
@@ -8,8 +9,10 @@ from .pagination import MyPagination
 class ProductListAPI(generics.ListAPIView):
     queryset=Product.objects.all()
     serializer_class=serializers.ProductListSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend,filters]
     filterset_fields = ['brand', 'flag']
+    search_fields = ['name', 'subtitle']
+
 
 class ProductDetailAPI(generics.RetrieveAPIView):
     queryset=Product.objects.all()
@@ -20,6 +23,8 @@ class BrandListAPI(generics.ListAPIView):
     queryset=Brand.objects.all()
     serializer_class=serializers.BrandListSerializer
     pagination_class=MyPagination  #custom pagaintion
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name' ]
 
 
 class BrandDetailAPI(generics.RetrieveAPIView):
