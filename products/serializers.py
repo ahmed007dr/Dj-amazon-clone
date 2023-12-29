93952db49dd447adabcd230e13344b9afc177c8b
ahmed007dr <ahmed007eg@gmail.com>
2023-12-29 13:37:06 +0200

    use model instance methods in serializers

--- products/serializers.py
+++ products/serializers.py
@@ -21,19 +21,11 @@ class ProductListSerializer(serializers.ModelSerializer):
         fields='__all__'
 
     def get_reviews_count(self,object): #name of function (get_)+ name of column # self couz in class #object product activae
-        reviews = object.review_product.all().count()
+        reviews = object.reviews_count()
         return reviews
     
     def get_avg_rate(self,object):
-        total=0 #sum rate: object (one product)
-        reviews=object.review_product.all()
-        
-        if len(reviews)>0:
-            for item in reviews:
-                total+= item.rate
-            avg= total / len(reviews)
-        else:
-            avg=0
+        avg=object.avg_rate()
         return avg
 
 class ProductDetailSerializer(serializers.ModelSerializer):
