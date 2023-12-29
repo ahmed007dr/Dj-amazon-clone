1636a81e22b8f4d0c3cf470761d02387ef306a4f
ahmed007dr <ahmed007eg@gmail.com>
2023-12-29 19:42:19 +0200

    try to use How to install redis on windows

--- settings/settings_context_processor.py
+++ settings/settings_context_processor.py
@@ -1,5 +1,9 @@
 from .models import Settings
+from django.views.decorators.cache import cache_page
 
+from context_cache.decorators import cache_for_context
+
+@cache_page(60 * 10 )
 def get_settings(request):
     data=Settings.objects.last()
     return{'settings_data':data}
