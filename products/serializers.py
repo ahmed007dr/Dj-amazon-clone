0591e187808333a137c60e675c8074ed0249f0f2
ahmed007dr <ahmed007eg@gmail.com>
2023-12-29 13:31:26 +0200

    show avg_review , review_count in template

--- products/serializers.py
+++ products/serializers.py
@@ -46,21 +46,7 @@ class ProductDetailSerializer(serializers.ModelSerializer):
         model=Product
         fields='__all__'
 
-    def get_reviews_count(self,object): #name of function (get_)+ name of column # self couz in class #object product activae
-        reviews = object.review_product.all().count()
-        return reviews
 
-    def get_avg_rate(self,object):
-        total=0 #sum rate: object (one product)
-        reviews=object.review_product.all()
-        
-        if len(reviews) > 0 :
-            for item in reviews:
-                total += item.rate
-            avg= total / len(reviews)
-        else:
-            avg=0
-        return avg
 
 class BrandListSerializer(serializers.ModelSerializer):
     class Meta:
