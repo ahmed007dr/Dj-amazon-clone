bc2b8132df300d6183f9fceaa8b6d01cd1f2575f
ahmed007dr <ahmed007eg@gmail.com>
2024-01-07 23:07:15 +0200

    loop for part start home page

--- settings/views.py
+++ settings/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-
+from django.db.models import Count
+from products.models import Product,Brand,Review
 # Create your views here.
 
 def home(request):
-    return render(request,'settings/home.html',{})
+    new_products=Product.objects.filter(flag='New')[:10]
+    sale_products=Product.objects.filter(flag='Sale')[:10]
+    feature_products=Product.objects.filter(flag='Feature')[:6]
+    brands=Brand.objects.annotate(product_count=Count('product_brand'))[:10]
+    review=Review.objects.all()
+
+
+    return render(request,'settings/home.html',{
+        'new_products':new_products,
+        'sale_products':sale_products,
+        'feature_products':feature_products,
+        'brands':brands,
+        'review':review,
+    })
