84821a641ec3d1374706255c73aee90fba347810
ahmed007dr <ahmed007eg@gmail.com>
2024-01-12 19:26:17 +0200

    38

--- orders/views.py
+++ orders/views.py
@@ -1,1 +1,10 @@
 from django.shortcuts import render
+from .models import Order,OrderDetails,Cart,CartDetails,Coupon
+
+def order_list(request):
+    data=Order.objects.filter(user=request.user)
+    return render(request,'orders/order_list.html',{'orders':data})
+
+
+def checkout(request):
+    return render(request,'orders/checkout.html',{})
