8b9fbcc48667e1d37f363a61a66c07568eb15a02
ahmed007dr <ahmed007eg@gmail.com>
2024-01-13 21:29:03 +0200

    edit error home capital and small

--- settings/views.py
+++ settings/views.py
@@ -4,14 +4,14 @@ from products.models import Product ,Brand ,Review
 # Create your views here.
 
 def home(request):
-    new_products = Product.objects.filter(flag='New')[:10]
-    sale_products = Product.objects.filter(flag='Sale')[:10]
-    feature_products = Product.objects.filter(flag='Feature')[:6]
+    new_products = Product.objects.filter(flag='new')[:10]
+    sale_products = Product.objects.filter(flag='sale')[:10]
+    feature_products = Product.objects.filter(flag='feature')[:6]
 
     brands = Brand.objects.annotate(product_count=Count('product_brand'))[:10]
     review = Review.objects.all()
 
-
+    print(sale_products)
     return render(request,'settings/home.html',{
         'new_products':new_products,
         'sale_products':sale_products,
