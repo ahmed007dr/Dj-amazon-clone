8071552fde65353afed5ad68dd437aa679ad9acb
ahmed007dr <ahmed007eg@gmail.com>
2024-01-13 10:30:39 +0200

    error in view order handdel

--- orders/views.py
+++ orders/views.py
@@ -10,20 +10,21 @@ def order_list(request):
 def checkout(request):
     return render(request,'orders/checkout.html',{})
 
-def add_to_cart(request):#كدا استلمنا من الفورم
-    product=product.objects.get(id=request.POST('product_id'))
-    quantity=int(request.POST['quantity'])
+def add_to_cart(request):
+    product_id = request.POST['product_id']
+    quantity = int(request.POST['quantity'])
 
-    cart=Cart.objects.get(user=request.user,status='Inprogress')
-    cart_details , created = CartDetails.objects.get_or_create(cart=cart,product=product)
+    # Use a different name for the variable representing the model
+    product_instance = Product.objects.get(id=product_id)
+
+    cart = Cart.objects.get(user=request.user, status='Inprogress')
+    cart_details, created = CartDetails.objects.get_or_create(cart=cart, product=product_instance)
 
     # if not created:
     #     cart_details.quatity=cart_details.quatity+quantity # if want to make more + 
 
-    cart_details.quatity=quantity #delete old quantity and add new qunatity
-    cart_details.total=round(product.price*cart_details.quantity,2)
+    cart_details.quantity = quantity  # delete old quantity and add new quantity
+    cart_details.total = round(product_instance.price * cart_details.quantity, 2)
     cart_details.save()
-    return redirect(f'/products/{product.slug}')
 
-
-
+    return redirect(f'/products/{product_instance.slug}')
