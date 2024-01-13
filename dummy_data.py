54d801e3e3b311fed04b5c46632953b9efb53bec
ahmed007dr <ahmed007eg@gmail.com>
2024-01-13 22:59:12 +0200

    edit name to new in dummy data handdel error home

--- dummy_data.py
+++ dummy_data.py
@@ -19,7 +19,7 @@ def seed_brand(n):
 
 def seed_prouducts(n):
     fake = Faker()
-    flag_types=['name','sale','feature']
+    flag_types=['new','sale','feature']
     brands=Brand.objects.all()
     image=['01.jpg','02.jpg','03.jpg','04.jpg','05.jpg','06.jpg','07.jpg','08.jpg','09.jpg','10.jpg']
 
@@ -40,4 +40,4 @@ def seed_reviews(n):
     pass
 
 #seed_brand(200)
-seed_prouducts(1500)
+#seed_prouducts(1500)
