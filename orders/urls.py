c96e444794c8425699ad6e168d7c289c4e3b4725
ahmed007dr <ahmed007eg@gmail.com>
2024-01-26 16:43:04 +0200

    add order list & details API

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from .views import order_list , checkout , add_to_cart
-
+from .api import OrderListAPI ,OrderDeailAPI
 
 urlpatterns = [
     path('',order_list),
     path('checkout/',checkout),  
     path('add-to-cart',add_to_cart),
+
+    #api
+    path('api/<str:username>/orders', OrderListAPI.as_view()),
+    path('api/<str:username>/orders/<int:pk>',OrderDeailAPI.as_view())
 
 ]
