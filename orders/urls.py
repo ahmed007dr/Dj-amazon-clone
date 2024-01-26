fb017520399f2745b344f6fb11be207e2105cffc
ahmed007dr <ahmed007eg@gmail.com>
2024-01-26 16:59:21 +0200

    add apply coupon api

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import order_list , checkout , add_to_cart
-from .api import OrderListAPI ,OrderDeailAPI
+from .api import OrderListAPI ,OrderDeailAPI ,ApplyCouponAPI
 
 urlpatterns = [
     path('',order_list),
@@ -9,6 +9,7 @@ urlpatterns = [
 
     #api
     path('api/<str:username>/orders', OrderListAPI.as_view()),
-    path('api/<str:username>/orders/<int:pk>',OrderDeailAPI.as_view())
+    path('api/<str:username>/orders/<int:pk>',OrderDeailAPI.as_view()),
+    path('api/<str:username>/apply-coupon', ApplyCouponAPI.as_view()),
 
 ]
