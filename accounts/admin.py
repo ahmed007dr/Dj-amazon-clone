820607baa6191b6adfb8ada594894d33b29b9d55
ahmed007dr <ahmed007eg@gmail.com>
2024-01-27 17:05:43 +0200

    desig profile models & add signals

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Address
+from .models import Address , Profile,ContactNumbers
 # Register your models here.
 
 admin.site.register(Address)
+admin.site.register(Profile)
+admin.site.register(ContactNumbers)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def signup(request):
+    pass
+    '''
+    create new user
+    send email : code
+    redirect : activatee
+    '''
+
+def user_activate(request):
+    pass
+    '''
+    -code ---> activate
+    -redirect ---> log in
+    '''
