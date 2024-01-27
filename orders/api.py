d1efc8a6566f55227b883a1ed600096259584e88
ahmed007dr <ahmed007eg@gmail.com>
2024-01-27 12:29:15 +0200

    add create order api

--- orders/api.py
+++ orders/api.py
@@ -3,12 +3,14 @@ from rest_framework.response import Response
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 import datetime
+from rest_framework import status
 
 
 from .serializers import CartDetailSerializer,CartSerializer,OrderDetailSerializer,OrderDetails,OrderSerializer
 from .models import Order,OrderDetails,Cart,CartDetails,Coupon
 from products.models import Product
 from settings.models import DeliveryFee
+from accounts.models import Address
 
 
 class OrderListAPI(generics.ListAPIView):
@@ -39,9 +41,8 @@ class OrderDeailAPI(generics.RetrieveAPIView):
     serializer_class=OrderSerializer
     queryset = Order.objects.all()
 
-
-
 class ApplyCouponAPI(generics.GenericAPIView):
+
     def post(self,request,*args, **kwargs):
         user=User.objects.get(username=self.kwargs['username']) # take it from url
         copoun=get_object_or_404(Coupon,code=request.data['coupon_code']) # take it from request body
@@ -63,4 +64,48 @@ class ApplyCouponAPI(generics.GenericAPIView):
             
             else:
                 return Response({'message':'coupon is not sucssufly'})
-        return Response({'message':'copoun not found'})
+        return Response({'message':'copoun not found'},status=status.HTTP_200_OK)
+    
+
+class CreateOrderAPI(generics.GenericAPIView):
+    def post(self,request,*args, **kwargs):
+        user=User.objects.get(username=self.kwargs['username']) # take it from url
+        code=request.data['payment_code']
+        address=request.data['address_id']
+
+        cart = Cart.objects.get(user, status='Inprogress')
+        cart_detail=CartDetails.objects.filter(cart=cart)
+        user_address=Address.objects.get(id=address)
+        #cart:order | cart_detail : order_detail
+        new_order=Order.objects.create(
+            user=user,
+            status='Received',
+            code = code,
+            address=user_address,
+            coupon=cart.coupon,
+            total_with_coupon=cart.total_with_coupon,
+            total=cart.cart_total
+        )
+        #order_details
+        for item in cart_detail:
+            product=Product.objects.get(id=item.product.id)
+            OrderDetails.objects.create(
+                order=new_order,
+                product=product,
+                quantity=item.quantity,
+                price=product.price,
+                total=round(item.quantity * product.price,2)
+            )
+            #decrease proudct quntity
+            product.quantity-= item.quantity
+            product.save()
+        #close cart
+        cart.status = 'Completed'
+        cart.save()
+
+        #send email
+        return Response ({'messge':'order was create successfully'},status=status.HTTP_201_CREATED)
+
+class CartCreateUpdateDelete(generics.GenericAPIView):
+    pass
+
