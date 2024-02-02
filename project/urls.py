60b01f7ef5dcf6733dbef69b6adef3e79cfc9ff3
ahmed007dr <ahmed007eg@gmail.com>
2024-02-02 21:22:48 +0200

    add auth with django_rest_auth

--- project/urls.py
+++ project/urls.py
@@ -23,6 +23,7 @@ from django.urls import re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 ...
 
@@ -53,6 +54,10 @@ urlpatterns = [
 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    #path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    #path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api-auth/', include('dj_rest_auth.urls')),
+    path('api-auth/registration/', include('dj_rest_auth.registration.urls'))
 
 
 ]
