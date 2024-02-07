8affe9187324432e05c8d0b95e09aa056257dc17
ahmed007dr <ahmed007eg@gmail.com>
2024-02-07 19:58:52 +0200

    add dashbord chart

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import signup, user_activate
+from .views import signup, user_activate,dashbord
 
 urlpatterns = [
     path('signup', signup),
+    path('dashbord', dashbord),
     path('<str:username>/activate', user_activate),
 ]
